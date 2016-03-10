09a898cd729efa7a198c1fcc07b7c1d88dcbaac5
piekos <piekos@greeny.cs.tlu.ee>
2016-03-10 11:20:41 +0200

    added deleting

--- app.php
+++ app.php
@@ -61,7 +61,7 @@
 		// 2 username
 		// 3 password
 		// 4 database
-		$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_romil");
+		$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_piekos");
 		
 		$stmt = $mysql->prepare("INSERT INTO messages_sample (recipient, message) VALUES (?,?)");
 			
